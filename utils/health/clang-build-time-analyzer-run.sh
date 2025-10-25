63c8cdf0bdd126aa5e9a0bf5df5e36807967fd6d
PitDaBull <pitdabull83@gmail.com>
2025-10-25 18:22:25 -0400

    Anero - Initial commit
    The Anero Project - This is the first intial commit for Anero. Forked from The Monero Project. Commit details: Updated clang-build-time-analyzer-run.sh to represent The Anero Network.

--- utils/health/clang-build-time-analyzer-run.sh
+++ utils/health/clang-build-time-analyzer-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Copyright (c) 2014-2024, The Monero Project
+# Copyright (c) 2024-2025, The Anero Project
 #
 # All rights reserved.
 #
@@ -28,14 +28,14 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# ClangBuildAnalyzer is able to analyze the aggregate build time of particular headers.
+# ClangBuildAnalyzer is used to analyze aggregate compilation time for specific headers.
 
 DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Build variables
 PROG="ClangBuildAnalyzer"
 PROG_PATH="$DIR_THIS/bin/$PROG"
-DIR_BUILD="build/clang-build-analyser"
+DIR_BUILD="build/clang-build-analyzer"
 
 # ClangBuildAnalyzer variables
 DIR_MONITORED="."
@@ -43,12 +43,12 @@ RESULT="cba-result.txt"
 TRACE="cba-trace.txt"
 
 if [ -f "$PROG_PATH" ]; then
-	echo "Found: $PROG_PATH"
+    echo "Found: $PROG_PATH"
 else
-	echo "Couldn't find: $PROG_PATH"
-	echo "Please run the below script to clone and build $PROG:"
-	echo "$DIR_THIS/build-scripts/clang-build-time-analyzer-clone-build.sh"
-	exit 1
+    echo "Couldn't find: $PROG_PATH"
+    echo "Please run the following script to clone and build $PROG:"
+    echo "$DIR_THIS/build-scripts/clang-build-time-analyzer-clone-build.sh"
+    exit 1
 fi
 
 mkdir -p "$DIR_BUILD" && cd "$DIR_BUILD"
@@ -61,15 +61,14 @@ cmake ../.. \
 -DBUILD_SHARED_LIBS=ON \
 -DBUILD_TESTS=ON
 
-make clean 				# Clean up, so that the trace can be regenerated from scratch
-$PROG_PATH --start $DIR_MONITORED	# Start monitoring
-time make 				# Build
-#time make easylogging			# Quick testing: build a single target
-$PROG_PATH --stop $DIR_MONITORED $TRACE	# Stop and output to trace file
-$PROG_PATH --analyze $TRACE | tee $RESULT # Analyze the trace, and store it in a readable format
-gzip -f $TRACE # Zip the trace, because it's huge. -f overwrites the previously generated trace
+make clean                                  # Clean to allow trace regeneration
+$PROG_PATH --start $DIR_MONITORED           # Start monitoring
+time make                                   # Build target
+#time make easylogging                      # Quick test target example
+$PROG_PATH --stop $DIR_MONITORED $TRACE     # Stop and dump trace
+$PROG_PATH --analyze $TRACE | tee $RESULT   # Analyze trace and store results
+gzip -f $TRACE                              # Compress large trace file (overwrite if exists)
 
 echo ""
 echo "Readable result stored in: $DIR_BUILD/$RESULT"
-echo "The trace (analyser's input data) in: $DIR_BUILD/$TRACE.gz"
-
+echo "Analyzer trace stored in:  $DIR_BUILD/$TRACE.gz"
