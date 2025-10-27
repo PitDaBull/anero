64a20deadbe6dc4cd63910dce73e404afab9129a
FireBlastAn0n <fireblast286@gmail.com>
2025-10-26 20:36:20 -0400

    Anero Network - Initial Recompile of all Files to Match Anero Variables

--- generate_translations_header-prefix/tmp/generate_translations_header-mkdirs.cmake
+++ generate_translations_header-prefix/tmp/generate_translations_header-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "/home/onyxnode/Projects/The Anero Project/anero/translations"
-  "/home/onyxnode/Projects/The Anero Project/anero/translations"
-  "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix"
-  "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/tmp"
-  "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/src/generate_translations_header-stamp"
-  "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/src"
-  "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/src/generate_translations_header-stamp"
+  "/home/onyxnode/anero/translations"
+  "/home/onyxnode/anero/translations"
+  "/home/onyxnode/anero/generate_translations_header-prefix"
+  "/home/onyxnode/anero/generate_translations_header-prefix/tmp"
+  "/home/onyxnode/anero/generate_translations_header-prefix/src/generate_translations_header-stamp"
+  "/home/onyxnode/anero/generate_translations_header-prefix/src"
+  "/home/onyxnode/anero/generate_translations_header-prefix/src/generate_translations_header-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/src/generate_translations_header-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/home/onyxnode/anero/generate_translations_header-prefix/src/generate_translations_header-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "/home/onyxnode/Projects/The Anero Project/anero/generate_translations_header-prefix/src/generate_translations_header-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/home/onyxnode/anero/generate_translations_header-prefix/src/generate_translations_header-stamp${cfgdir}") # cfgdir has leading slash
 endif()
